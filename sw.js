ee3c779d956afb5deb891492c3c2fad8d5995670
connormortoncbtx-sketch <connormortoncbtx@gmail.com>
2026-03-18 13:03:10 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'forge-v24';
+const CACHE = 'forge-v27';
 const PRECACHE = [
   'workout-tracker.html','exercise-library.js','manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.15.2/Sortable.min.js',
